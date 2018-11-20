c10ff48d58241d71eddd31497eaf11063b7c23dd
Andre Kerlin <kerlinandre@gmail.com>
2018-11-19 21:11:33 -0500

    fixed database isssues with featured artist.php

--- technicallysound(1)/Smarty_Example_2/public_html/featuredArtist.php
+++ technicallysound(1)/Smarty_Example_2/public_html/featuredArtist.php
@@ -9,6 +9,30 @@
 //$Smarty = new Smarty;
 
 require_once "../private_html/config.inc.php";
+require_once "../private_html/dbconfig.inc.php";
 //{include file = "templates/FeaturedArtist.tpl"}
 
+$get_artist = "SELECT *
+	FROM Artist";
+
+$stmt = $pdo->prepare($get_artist);
+$stmt->execute();
+
+$artist_list = array();
+while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $artist_list[] = $row;
+}
+if(isset($_POST["search2"])) {
+    $search_var = '%'.$_POST["search2"].'%';
+    $sql2 = "SELECT * FROM Artist WHERE Artist_Name LIKE ':search'";
+    $stmt2 = $pdo->prepare($sql2);
+    $stmt2->bindParam(':search', $search_var);
+    $stmt2->execute();
+} else {
+    $message = "";
+    $smarty->assign("message", $message);
+}
+
+$smarty->assign("artist_list", $artist_list);
+$smarty->assign("active", "Artists");
 $smarty->display('FeaturedArtists.tpl');
